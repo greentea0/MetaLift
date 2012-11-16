3123c2137b4bf9eb2da7f0edf331371fa4f91ae0
Alex Kwan <alexkwan089@gmail.com>
2012-11-15 20:26:11 -0800

    saving initial db changes in boot.scala

--- src/main/scala/bootstrap/liftweb/Boot.scala
+++ src/main/scala/bootstrap/liftweb/Boot.scala
@@ -57,7 +57,7 @@ class Boot {
     // Use Lift's Mapper ORM to populate the database
     // you don't need to use Mapper to use Lift... use
     // any ORM you want
-    Schemifier.schemify(true, Schemifier.infoF _, User)
+    Schemifier.schemify(true, Schemifier.infoF _, User, Conversation, FriendsList, History, IgnoredList, Message)
 
     // where to search snippet
     LiftRules.addToPackages("code")
