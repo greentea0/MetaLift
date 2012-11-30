4ded984d04f4df93ca7b5588a7a0edb24c4eb1df
Alex Kwan <alexkwan089@gmail.com>
2012-11-30 08:59:10 -0800

    Added in trends to trends.html
    they are only trends of that user

---
+++ src/main/scala/code/comet/CurrentTrends.scala
@@ -0,0 +1,56 @@
+package code
+package comet
+import net.liftweb._
+import http._
+import util._
+import Helpers._
+import java.sql.Date
+import code.model._
+import code.model.Message
+import java.util.Calendar
+import net.liftweb.mapper._
+import net.liftweb.mapper.MappedLongIndex
+import net.liftweb.http.js.JE
+/**
+ * The screen real estate on the browser will be represented
+ * by this component.  When the component changes on the server
+ * the changes are automatically reflected in the browser.
+ */
+class CurrentTrends extends CometActor with CometListener {
+  private var trends: List[String] = List() // private state
+  /**
+   * When the component is instantiated, register as
+   * a listener with the ChatServer
+   */
+  def registerWith = ChatServer
+  /**
+   * The CometActor is an Actor, so it processes messages.
+   * In this case, we're listening for Vector[String],
+   * and when we get one, update our private state
+   * and reRender() the component.  reRender() will
+   * cause changes to be sent to the browser.
+   */
+  override def lowPriority = {
+  	case s: String => 
+	 
+  	val tempTrends = Trend.findAll(By( Trend.user, User.currentUser)
+  	    , By(Trend.reportType, TrendServer.HOURLY)
+  	    , OrderBy(Trend.occurrence, Descending))
+  //	 val tempTrends = Trend.findAll(By( Trend.user, User.currentUser),
+ // 	     OrderBy(Trend.occurrence, Descending)).
+  	      println( "This is temp Trends "+ tempTrends)
+	 trends = tempTrends.map( ( t : Trend ) => TrendWord.find( By ( TrendWord.id, t.word)).get.word.get)
+	println(trends)
+	reRender()
+   
+  }
+  /**s
+   * Put the messages in the li elements and clear
+   * any elements that have the clearable class.
+   */
+  def render = {
+    println("printing the trends "+trends)
+    "li *" #> trends & ClearClearable
+
+    }
+}
