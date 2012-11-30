9a17eaa074a73c1c8502ada69bc759223a8acce7
pavelpep <pdp@sfu.ca>
2012-11-30 08:27:51 -0800

    add loging to homepage

--- src/main/scala/code/model/User.scala
+++ src/main/scala/code/model/User.scala
@@ -16,8 +16,7 @@ object User extends User with MetaMegaProtoUser[User] {
   override def screenWrap = Full(<lift:surround with="default" at="content">
 			       <lift:bind /></lift:surround>)
   // define the order fields will appear in forms and output
-  override def fieldOrder = List(id, firstName, lastName, email,
-  locale, timezone, password, nickname, textArea)
+  override def fieldOrder = List(id, firstName, lastName, email, password, nickname, textArea)
 
   // comment this line out to require email validations
   override def skipEmailValidation = true
